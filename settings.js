4c850c76eda59be7cf4f355f9dfb9da0f339f027
sarKarji1 <149999688+sarKarji1@users.noreply.github.com>
2024-08-24 10:44:52 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,73 @@
+const chalk = require("chalk")
+const fs = require("fs")
+
+//aumto presence update
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.autoblockmorroco = false //auto block 212 (true to on, false to off)
+global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
+global.antispam = false //auto kick spammer (true to on, false to off)
+
+
+//if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
+global.keyopenai = "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"
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.owner = ['923253617422'] //ur owner number
+global.ownernomer = "923143200187" //ur owner number2
+global.ownername = "sarkar bandali" //ur owner name
+global.ytname = "YT: sarkar" //ur yt chanel name
+global.socialm = "GitHub: sarkarji" //ur github or insta name
+global.location = "pakistan, sindh, karachi" //ur location
+
+//new
+global.botname = "sarkar Bot MD V8"
+global.ownernumber = '923253617422'
+global.ownername = 'sarkar'
+global.ownerNumber = ["923253617422@s.whatsapp.net"]
+global.ownerweb = "https://youtube.com/@Sarkarjiteach"
+global.websitex = "texhbybandali.blogspot.com"
+global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.themeemoji = '🪀'
+global.wm = "Xeon Bot Inc."
+global.botscript = 'https://github.com/sarKarji1/SarkarBot-MD8' //script link
+global.packname = "Sticker By"
+global.author = "BANDAH E ALI\n\n"
+global.creator = "923253617422@s.whatsapp.net"
+global.prefa = ['','!','.','#','&']
+global.sessionName = 'session'
+global.hituet = 0
+
+//media target
+global.thum = fs.readFileSync("./XeonMedia/theme/sarkar.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/sarkar.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/sarkar.jpg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/sarkar.jpg") //ur thumb pic
+global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60' //default pp wa
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//messages
+global.mess = {
+    success: 'Here you go!', 
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
---
+++ xeon.js
@@ -0,0 +1,3 @@
+{
+	"name": "Sarkar Bot Multi Device "
+}
